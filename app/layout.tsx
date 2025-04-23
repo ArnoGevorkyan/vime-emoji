aa5e4c81d9a07170c8120d83cda9a286a9b3945f
Arno Gevorkyan <hello@carty.cc>
2025-04-23 14:42:21 -0400

    datafast

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <script
+          defer
+          data-website-id="68093411bfcb36a1a6686c40"
+          data-domain="vimeworld.vercel.app"
+          src="https://datafa.st/js/script.js">
+        </script>
       </head>
       <body>{children}</body>
     </html>
